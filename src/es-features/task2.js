db8fab7bc60b7b01d5c970a9510f17d89be7fd35
Roman Krets <romankrets@yandex.com>
2023-04-06 16:09:34 +0700

    es-features task2 is done

--- src/es-features/task2.js
+++ src/es-features/task2.js
@@ -16,4 +16,7 @@ export function task2Old(a, b) {
 // Напишите реализацию функции task2Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task2New() {}
+export function task2New() {
+  const [a, b, ...restData] = arguments;
+  return (a + b) * restData.length;
+}
