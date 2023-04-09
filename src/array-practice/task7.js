5e0aee8337bb5d5404a624be8556e799443a2afa
Roman Krets <romankrets@yandex.com>
2023-04-10 01:33:29 +0700

    array-practice task7 review1

--- src/array-practice/task7.js
+++ src/array-practice/task7.js
@@ -12,7 +12,7 @@
  * console.log(take([1, 2, 3], 2)); -> [1, 2]
  */
 export default function take(array, count) {
-  return array.splice(0, count);
+  return array.slice(0, count);
 }
 take([1, 2, 3], 5);
 take([1, 2, 3], 2);
